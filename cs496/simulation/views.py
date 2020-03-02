2b7e57f0fed1cb945ca19405e7abbefa29f23953
calebkincaid898 <caleb.kincaid898@topper.wku.edu>
2020-03-01 19:08:19 -0600

    Starting Website

---
+++ cs496/simulation/views.py
@@ -0,0 +1,8 @@
+from django.utils import timezone
+from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, redirect, render
+from django.db.models import Count
+from django.views.generic import UpdateView, ListView, CreateView
+def home(request):
+    return render(request, 'home.html')
